917cbc11ad2abde2cf4b57a6d3c6518c863baecd
dtorres <dtorres@vectorsf.com>
2016-11-26 13:32:52 +0100

    sadf

--- app.js
+++ app.js
@@ -1,34 +1,90 @@
 /*eslint-env node*/
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
+const erisDbFactory = require('eris-db');
+const erisContracts = require('eris-contracts');
+const solc = require('solc');
+const accounts = require("./accounts.js").accounts;
+const nodes = require("./ips.js").ips;
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
-var cfenv = require('cfenv');
+var erisdb; /* ErisDB Factory */
+var erisdbURL; /* ErisDB RPC URL */
+var pipe; /* Pipe for creating contracts */
+var contractManager;/* Contract Manager for creating contracts*/
+var account = accounts[0].address;
+var greeterSource = 'contract greeter { string greeting; function greeter(string _greeting) public { greeting = _greeting; } function greet() constant returns (string) { return greeting; } }'
+
+/*Initialize ERISDB*/
+erisdb = erisDbFactory.createInstance(nodes[0]);
+erisdb.start(function(error){
+    if(!error){
+        console.log("Ready to go");
+    }
+});
+
+pipe = new erisContracts.pipes.DevPipe(erisdb, accounts); /* Create a new pipe*/
+contractManager = erisContracts.newContractManager(pipe); /*Create a new contract object using the pipe */
+
+/*Get account list*/
+erisdb.accounts().getAccounts((err, res) => { console.log(res.accounts.map(item => {
+  return ({
+    ADDR: item.address,
+    BALANCE: item.balance
+  })
+})) });
+
+/* Compile the Greeter Contract*/
+var compiledContract = solc.compile(greeterSource);
+console.log("Compiled Contract:" + compiledContract)
+var contractFactory = contractManager.newContractFactory(JSON.parse(compiledContract.contracts.greeter.interface)); //parameter is abi
+console.log("Contract Factory:" + contractFactory)
+
+/* Send the contract */
+contractFactory.new.apply(contractFactory, ["Hello World",
+ {from: account, data:compiledContract.contracts.greeter.bytecode}, (err, contractInstance)=> {
+  console.log(contractInstance.address);
+  contractInstance["greet"].apply(contractInstance, [(error,result)=> {
+     if (error) {
+       console.log(error);
+     }
+    else {
+      console.log(result);
+    }
+  }]);
+
+ }]);
+
+
+
+
+
+// Load the appropriate modules for the app
+var cfenv = require("cfenv");
+var express = require("express");
 var bodyParser = require('body-parser');
-var blockChain = require('./erisDBApi');
 
-// get the app environment from Cloud Foundry
+// Defensiveness against errors parsing request bodies...
+process.on('uncaughtException', function (err) {
+    console.log('#### Caught exception: ' + err);
+});
+process.on("exit", function(code) {
+    console.log("#### Exiting with code: " + code);
+});
+
+// Checking Bluemix setup
 var appEnv = cfenv.getAppEnv();
 
 // Configure the app web container
 var app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
 
-// start server on the specified port and binding host
+// Finishing configuration of the app web container
 app.listen(appEnv.port);
-// print a message when the server starts listening
-console.log("server starting on " + appEnv.port);
-
-//blockChain.init();
+console.log("#### Server listening on port " + appEnv.port);
 
 // Home page
-app.get('/', function (req, res) {
-      //console.log("Init Page, contract: " + blockChain.getContract());
-      res.render('index', {compiledContract: "{hola}"});
+app.get('/', function (req, res)
+{
+      res.render('index', {compiledContract: compiledContract});
 });
---
+++ back.js
@@ -0,0 +1,34 @@
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require('express');
+var cfenv = require('cfenv');
+var bodyParser = require('body-parser');
+var blockChain = require('./erisDBApi');
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// Configure the app web container
+var app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/views'));
+
+// start server on the specified port and binding host
+app.listen(appEnv.port);
+// print a message when the server starts listening
+console.log("server starting on " + appEnv.port);
+
+//blockChain.init();
+
+// Home page
+app.get('/', function (req, res) {
+      //console.log("Init Page, contract: " + blockChain.getContract());
+      res.render('index', {compiledContract: "{hola}"});
+});
