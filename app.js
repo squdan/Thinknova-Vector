fe9fd8cb945e47dee1b503f8f72ca0e8b4cf645d
dtorres <dtorres@vectorsf.com>
2016-11-26 12:38:06 +0100

    Cambios hechos para que se muestren los contratos en el index.

--- app.js
+++ app.js
@@ -7,22 +7,18 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
-
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
-
-// create a new express server
-var app = express();
-
-// BlockChain API
+var bodyParser = require('body-parser');
 var blockChain = require('./erisDBApi');
-
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
+
+// Configure the app web container
+var app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/views'));
 
 // start server on the specified port and binding host
 app.listen(appEnv.port);
